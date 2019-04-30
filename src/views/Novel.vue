787648768bcd6039337a96af83d2c123c648f71c
hide6644 <hide6644@gmail.com>
2019-04-30 15:24:29 +0900

    Initial commit

--- src/views/Novel.vue
+++ src/views/Novel.vue
@@ -5,15 +5,46 @@
         <div slot="header" class="clearfix">
           <span>小説一覧</span>
         </div>
-        <div>...</div>
+        <el-table
+            :data="novels"
+            style="width: 100%">
+          <el-table-column
+              prop="title"
+              label="タイトル" />
+          <el-table-column
+              prop="writername"
+              label="作者" />
+          <el-table-column
+              prop="description"
+              label="解説" />
+        </el-table>
       </el-card>
     </el-col>
   </el-row>
 </template>
 
 <script>
+  /* eslint-disable no-console */
+
+  import axios from 'axios'
+
   export default {
-    name: "Novel"
+    name: "Novel",
+    data () {
+      return {
+        novels: []
+      }
+    },
+    created: async function () {
+      await this.refresh()
+    },
+    methods: {
+      refresh: async function () {
+        const res = await axios.get('http://localhost:8181/novels')
+        this.novels = res.data.novels
+        console.info(this.novels)
+      }
+    }
   }
 </script>
 
